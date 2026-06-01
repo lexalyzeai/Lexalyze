facdaf887763fa2c7dfd25980876e9777acb5d1a
Rishaan <rishaan.keswani@gmail.com>
2026-06-01 16:29:29 +0530

    reduce cloudflare worker bundle size

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-import { withSentryConfig } from "@sentry/nextjs";
 
 initOpenNextCloudflareForDev();
 
@@ -16,36 +15,6 @@ const nextConfig = {
     NEXT_PUBLIC_SITE_URL: productionSiteUrl,
     NEXT_PUBLIC_CLOUDFLARE_URL: process.env.NEXT_PUBLIC_CLOUDFLARE_URL,
   },
-}
+};
 
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-  org: "lexalyze",
-
-  project: "lexalyze",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
-  webpack: {
-    // Tree-shaking options for reducing bundle size
-    treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      removeDebugLogging: true,
-    },
-  },
-});
+export default nextConfig;
